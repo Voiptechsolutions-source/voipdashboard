523f9557a9b433d611468073e5d7217bfbb4dc8f
sumiti <suthar05october@gmail.com>
2025-03-27 14:29:32 +0530

    webhook

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Customer\CustomerApiController; // Updated namespace
 use App\Http\Controllers\WebhookController;
-use App\Http\Controllers\FacebookWebhookController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -12,7 +11,6 @@ Route::get('/user', function (Request $request) {
 
 Route::post('/savelead', [CustomerApiController::class, 'savecustomerlead']);
 Route::post('/webhook/google-ads', [WebhookController::class, 'handle']);
-Route::post('/facebook-webhook', [FacebookWebhookController::class, 'handleWebhook']);
 
 
 
